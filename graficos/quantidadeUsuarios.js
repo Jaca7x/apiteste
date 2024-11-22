89a7b23e86f328bcb95e916dca1f856fdca072dd
Jaca1x <117647243+Jaca7x@users.noreply.github.com>
2024-11-22 14:08:37 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig, criarGrafico } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/Jaca7x/apiteste/refs/heads/main/api2'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
