fe74e4cf5d637dd7312a48d4c37a80fd11ab0c01
Jaca1x <117647243+Jaca7x@users.noreply.github.com>
2024-11-22 14:08:21 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/Jaca7x/apiteste/refs/heads/main/api3'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
